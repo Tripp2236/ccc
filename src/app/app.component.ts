fc3708a2b0aa8459af8537efeea53235da4df34b
Ryan Brown <ryanbrown36812@gmail.com>
2022-07-07 11:05:58 -0400

    CCC initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ccc';
+  title = 'Charity Christain Church of Barberton';
 }
