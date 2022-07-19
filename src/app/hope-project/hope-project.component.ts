f92c8bb1ca073d6db83c3ed17a36ff34af2b3b91
tripp <ryanabrown6813@yahoo.com>
2022-07-19 10:27:55 -0400

    Color Updates
    updated color for pages, font color, and background colors.

--- src/app/hope-project/hope-project.component.ts
+++ src/app/hope-project/hope-project.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Video } from '../model/video';
 @Component({
   selector: 'app-hope-project',
   templateUrl: './hope-project.component.html',
